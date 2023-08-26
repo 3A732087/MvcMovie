0d5b3abdb1f46953430c96895596e0121071f4db
Quannn <quannn0180716v@gmail.com>
2023-08-26 14:44:30 +0800

    新增Controller練習

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+
+        //
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    //HtmlEncoder -> Html編碼方式，保護應用程式免于惡意輸入，例如透過 JavaScript。
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
